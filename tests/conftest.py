71fb11b489fa384cbb9cac4e97bf665f26761ff9
Tim Schönborn <tim.schoenborn@nebul.com>
2024-11-26 23:05:35 +0100

    added some more sample tests and fixtures to demo testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,15 +7,43 @@ def sample_document():
     return Document(
         content="This is a test document.",
         chunks=[],
-        metadata={"source": "test"},
+        metadata={"source": "test.pdf"},
+    )
+
+
+@pytest.fixture
+def sample_document_with_chunk():
+    return Document(
+        content="This is a test document.",
+        chunks=[sample_chunk()],
+        metadata={"source": "test.pdf"},
+    )
+
+
+@pytest.fixture
+def sample_document_with_two_chunks():
+    return Document(
+        content="This is a test document.",
+        chunks=[sample_chunk(), sample_chunk_2()],
+        metadata={"source": "test.pdf"},
     )
 
 
 @pytest.fixture
 def sample_chunk():
     return Chunk(
-        content="This is a test chunk.",
+        content="This is the first test chunk.",
         start_index=0,
         end_index=10,
-        metadata={"position": "first"},
+        metadata={"position": 0},
+    )
+
+
+@pytest.fixture
+def sample_chunk_2():
+    return Chunk(
+        content="This is the second test chunk.",
+        start_index=11,
+        end_index=20,
+        metadata={"position": 1},
     )
